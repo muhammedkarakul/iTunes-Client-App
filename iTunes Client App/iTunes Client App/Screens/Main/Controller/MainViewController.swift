c84e76896fe3b951a0ab745125dff560724c6a04
Pazarama iOS Bootcamp <>
2022-10-01 11:52:49 +0300

    iTunesAPI added.

--- iTunes Client App/iTunes Client App/Screens/Main/Controller/MainViewController.swift
+++ iTunes Client App/iTunes Client App/Screens/Main/Controller/MainViewController.swift
@@ -11,9 +11,19 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .white
+        
+        iTunesAPI.shared.fetchPodcasts() { response, error in
+            if let error = error {
+                print(error)
+                return
+            }
+            guard let response = response else {
+                return
+            }
+            print(response)
+        }
     }
-
-
 }
 
